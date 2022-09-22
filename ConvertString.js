459076be725d200d4bb330988c1e3bed41293a3a
Chris <114193486+ChadwickCJ@users.noreply.github.com>
2022-09-22 23:50:56 +0100

    Update ConvertString.js

--- ConvertString.js
+++ ConvertString.js
@@ -7,12 +7,9 @@ Shelly.call("Shelly.GetStatus",
             {id: 0},
             function (result, error_code, error_message, userdata)
             {
-//           print(JSON.stringify(result));
-//           print(JSON.parse(JSON.stringify(result.sys)).time);
              let SysStatus = JSON.stringify(result.sys);
              let SysInfo   = JSON.parse(SysStatus);
              let SysTime   = SysInfo.time;
-//           print (typeof SysStatus, typeof SysInfo, typeof SysTime);
              SetTime(SysTime);
             },
             null
@@ -23,7 +20,6 @@ function SetTime(MyTime) {
 let substr1 = MyTime.slice(0, 2);
 let substr2 = MyTime.slice(3, MyTime.length);
 let ObjectString = "{" + chr(34) + "TimeNum" + chr(34) + ":" + substr1 + substr2 + "}";
-//print(ObjectString);
 let TimeObj = JSON.parse(ObjectString);
 print (MyTime, typeof MyTime, TimeObj.TimeNum, typeof TimeObj.TimeNum);
 }

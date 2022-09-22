933d72ad9993da2b00d0ef6ebac72e4317bcbfb3
Chris <114193486+ChadwickCJ@users.noreply.github.com>
2022-09-22 23:43:10 +0100

    Shelly string convertor
    Takes time of day as a string and converts it to a number using JSON.

---
+++ ConvertString.js
@@ -0,0 +1,27 @@
+//Shelly.GetStatus returns multiple objects:
+//ws, wifi, sys, switch:0, script:n (depending on the number of scripts), mqtt, input:0, cloud, and ble.
+//This example retrieves "sys", which contains the time of the system as a string and convert it to a number.
+
+function DeviceStatus() {
+Shelly.call("Shelly.GetStatus",
+            {id: 0},
+            function (result, error_code, error_message, userdata)
+            {
+             let SysStatus = JSON.stringify(result.sys);
+             let SysInfo   = JSON.parse(SysStatus);
+             let SysTime   = SysInfo.time;
+             SetTime(SysTime);
+            },
+            null
+);
+}
+
+function SetTime(MyTime) {
+let substr1 = MyTime.slice(0, 2);
+let substr2 = MyTime.slice(3, MyTime.length);
+let ObjectString = "{" + chr(34) + "TimeNum" + chr(34) + ":" + substr1 + substr2 + "}";
+let TimeObj = JSON.parse(ObjectString);
+print (MyTime, typeof MyTime, TimeObj.TimeNum, typeof TimeObj.TimeNum);
+}
+
+DeviceStatus()

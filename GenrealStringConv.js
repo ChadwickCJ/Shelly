452029363b0f3ba070294697d98800d0f0a0521d
Chris <114193486+ChadwickCJ@users.noreply.github.com>
2022-09-27 18:18:42 +0100

    Create GenrealStringConv.js

---
+++ GenrealStringConv.js
@@ -0,0 +1,17 @@
+// Strips non-numeric characters from a string to return a number.
+
+let allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function convertString(theString){
+  let stringTheory = "";
+  for (let i = 0; i < theString.length; i++) {
+    if (theString[i] === allNumbers[theString[i]]) {
+      stringTheory += theString[i];
+    }
+  }
+  let objectString = "{\"myNum\":" + stringTheory + "}";
+  return JSON.parse(objectString).myNum;
+}
+
+print(convertString("x1we1:23zzz"));
+print(convertString("10034fwsrf346ue5y0"));

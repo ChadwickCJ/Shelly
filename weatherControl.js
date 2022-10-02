ed57fa190a872f4f208be5d734cddddfdd1aef64
Chris <114193486+ChadwickCJ@users.noreply.github.com>
2022-10-02 12:20:36 +0100

    Update weatherControl.js

--- weatherControl.js
+++ weatherControl.js
@@ -1,18 +1,18 @@
 // This switches a device on or off based on % cloud cover between two time periods.
-// The cache expires time from AccuWeather is used to prevent requesting data before the next refresh.
+// The cache expires time from AccuWeather is used to prevent requesting data before the next forecast is due.
 // CONFIG.startTime is overwritten with this value to provide the control.
 
 let CONFIG = {
-  accuWeatherURL: "http://dataservice.accuweather.com/",
   accuWeatherAPIKey: "--- You need your own key from AccuWeather ---",
+  accuWeatherURL: "http://dataservice.accuweather.com/",
   currentURL: "currentconditions/v1/",
   cityKey: "49908_PC",
   getInterval: 1 * 60 * 1000, //1 minute as milliseconds
   repeat: true,
   deviceId: 0,
   cloudMinimum: 20,
-  startDefault: 1145,
-  startTime: 1145,
+  startDefault: 1100,
+  startTime: 1100,
   stopTime: 1400
 };
 
@@ -25,7 +25,7 @@ function deviceStatus(IdNum) {
                if (isItTime(deviceTime)) {
                  print("Time for cloud check!")
                  getClouds();
-                 print("Did it still work", CONFIG.startTime);
+               //print("Did it still work", CONFIG.startTime);
                } else{
                  print(deviceTime, "is not the time.")
                }
@@ -35,6 +35,7 @@ function deviceStatus(IdNum) {
 }
 
 function convertString(theString) {
+//print("theString", JSON.stringify(theString));
   let allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   let stringTheory = "";
   for (let i = 0; i < theString.length; i++) {
@@ -62,18 +63,19 @@ function isItTime(thisTime) {
 function getClouds() {
   Shelly.call("http.get", 
              {url:CONFIG.accuWeatherURL + CONFIG.currentURL + CONFIG.cityKey +
-                   "?apikey=" + CONFIG.accuWeatherAPIKey + "&language=EN-GB&details=true"},
+                  "?apikey=" + CONFIG.accuWeatherAPIKey + "&language=EN-GB&details=true"},
               function (result) {
               let currentWeather = JSON.parse(result.body);
               if (currentWeather.Code !== "ServiceUnavailable") {
-                let expiresTs = result.headers["Expires"];
-                let expiresTm = expiresTs.slice(expiresTs.length - 12, expiresTs.length - 7);
-                print("Cache expires time:", expiresTs, expiresTm);
-                CONFIG.startTime = convertString(expiresTm) + 101; //Add 1 hour for GMT and round up minutes
-                print("Did it work", CONFIG.startTime, convertString(expiresTm))
-                print(currentWeather[0].LocalObservationDateTime);
-                print(currentWeather[0].CloudCover, "%");
-                if (currentWeather[0].CloudCover < CONFIG.cloudMinimum) {
+                let expiresTs = result.headers.Expires;
+                let expiresTm = convertString(expiresTs.slice(expiresTs.length - 12, expiresTs.length - 7));
+                let localODT = currentWeather[0].LocalObservationDateTime;
+                let timeOffset = convertString(localODT.slice(localODT.length - 5, localODT.length));
+                CONFIG.startTime = expiresTm + timeOffset + 1; //Add GMT offset and round up minutes
+                print("Cache expires time:", expiresTs);
+                print("Did it work", CONFIG.startTime, expiresTm)
+                print(localODT, currentWeather[0].CloudCover, "%");
+                if (currentWeather[0].CloudCover <= CONFIG.cloudMinimum) {
                   switchMode(true);
                 } else {
                   switchMode(false);
@@ -83,17 +85,6 @@ function getClouds() {
               }
               },
               CONFIG.startTime
-  );
-}
-
-function TimeZone() {
-  Shelly.call("Shelly.GetConfig",
-              {id: IdNum},
-              function (result)
-              {
-               let tZ= convertString(JSON.parse(JSON.stringify(result.location)).tz);
-              },
-              tZ
   );
 }
 

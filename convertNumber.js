22451e84f68be0bb77ecfe9fe6559fbde89f0ce4
Chris <114193486+ChadwickCJ@users.noreply.github.com>
2022-10-15 17:40:37 +0100

    Update convertNumber.js

--- convertNumber.js
+++ convertNumber.js
@@ -2,7 +2,7 @@
 //---------------------------------
 
 function convertNumber(theNumber) {
-  let roundingError = 0.0000001;
+  let roundingAdjustment = 0.000000000000001;
   let allNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   let stringTheory = "";
   let sign = "";
@@ -17,7 +17,13 @@ function convertNumber(theNumber) {
     sign = "-";
   }
   
-  base10Log = (Math.log(theNumber) / Math.log(10)) + roundingError;
+  //Impact of rounding adjustment; computed value is 2.999999999999999375, not 3:
+  //print((Math.log(1000) / Math.log(10)),
+  //      (Math.log(1000) / Math.log(10) + roundingAdjustment));
+  //print((Math.floor(Math.log(1000) / Math.log(10))),
+  //      (Math.floor(Math.log(1000) / Math.log(10) + roundingAdjustment)));
+
+  base10Log = (Math.log(theNumber) / Math.log(10)) + roundingAdjustment;
   numberOfDigits = Math.floor(base10Log) + 1;
   
   for (let i = numberOfDigits - 1; i > -1; i--) {
@@ -30,8 +36,8 @@ function convertNumber(theNumber) {
 }
 
 print(convertNumber(0));
-print(convertNumber(-1));
-print(convertNumber(123));
-print(convertNumber(-56932));
-print(convertNumber(335588));
+print(convertNumber(1), convertNumber(-1), convertNumber(9), convertNumber(-9));
+print(convertNumber(10), convertNumber(-10), convertNumber(99), convertNumber(-99));
+print(convertNumber(100), convertNumber(-100), convertNumber(999), convertNumber(-999));
+print(convertNumber(1000), convertNumber(-1000), convertNumber(9999), convertNumber(-9999));
 print(convertNumber(1234567890));
